8bc64e3c7b5817c5eb3ccfd992cfb94293baccd8
anhtnph06685 <anhtnph066855@fpt.edu.vn>
2020-09-15 16:13:38 +0700

    CrossOrigin

--- src/main/java/fpoly/graduation/project/web/rest/UserResource.java
+++ src/main/java/fpoly/graduation/project/web/rest/UserResource.java
@@ -9,14 +9,12 @@ import fpoly.graduation.project.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.xml.ws.Response;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/user")
 public class UserResource {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
